78fb9185d4a23137bc811673dd8577a3b6ffd5c4
Shahd-Samaneh <s12220578@stu.najah.edu>
2026-07-16 14:08:34 +0300

    create project

---
+++ src/data/requests.js
@@ -0,0 +1,16 @@
+export const requests = [
+  {
+    id: 1,
+    title: "Kitchen Sink Leak",
+    category: "Home",
+    status: "Pending",
+    date: "2026-07-16",
+  },
+  {
+    id: 2,
+    title: "Oil Change",
+    category: "Auto",
+    status: "Completed",
+    date: "2026-07-15",
+  },
+];
